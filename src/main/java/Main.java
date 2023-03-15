30e926fe3dab9eb87c5c07eb7a0c0112e3deabba
Mateusz <mateusz.pryl@gmail.com>
2023-03-15 11:18:00 +0100

    Reworked filling to backtracking algorithm and added different board initialization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         SudokuBoard sudokuBoard = new SudokuBoard();
         sudokuBoard.fillBoard();
-        sudokuBoard.PrintBoard();
 
     }
 }
--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -5,61 +5,51 @@ public class SudokuBoard {
     private Integer[][] board;
 
     public SudokuBoard() {
-        board = new Integer[9][9];
+
+        board = new Integer[][]
+                {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}};
     }
 
-    public void PrintBoard() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-                if (j == 2 || j == 5)
-                    System.out.print("| ");
-            }
-            System.out.println();
-            if (i == 2 || i == 5)
-                System.out.println("---------------------");
-        }
-    }
     public void fillBoard() {
-        Random rand = new Random();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = 0;
-            }
-        }
-        //fill 15 random numbers
-        for (int k = 0; k < 15; k++) {
-            int i = rand.nextInt(9);
-            int j = rand.nextInt(9);
-            int num = rand.nextInt(9) +1;
-            board[i][j] = num;
-        }
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                System.out.print(board[i][j]+" ");
-                if(j==2 || j==5)
-                    System.out.print("| ");
-            }
-            System.out.println();
-            if(i==2 || i==5)
-                System.out.println("---------------------");
-        }
-        System.out.println();
+        // Backtracking algorithm
+        boolean isFull = true;
 
-        //fill the rest with the board
-        for (int i = 0; i < board.length; i++) {
+        for (Integer[] integers : board) {
             for (int j = 0; j < board.length; j++) {
-                int num = board[i][j];
-                if (num == 0) {
-                    for (int k = 0; k < 9; k++) {
-                        if (validation(i, j, k) == true) {
-                            board[i][j] = k;
-                            break;
-                        }
-                    }
+                if (integers[j] == 0) {
+                    isFull = false;
+                    break;
                 }
             }
         }
+
+        if (!isFull) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if (board[i][j] == 0) {
+                        for (int k = 1; k <= 9; k++) {
+                            if (validation(i, j, k)) {
+                                board[i][j] = k;
+                                fillBoard();
+                                board[i][j] = 0;
+                            }
+                        }
+                        return;
+                    }
+                }
+            }
+        } else {
+            printBoard();
+        }
+
     }
 
     private boolean validation(int row, int col, int num) {
@@ -83,5 +73,19 @@ public class SudokuBoard {
             }
         }
         return true;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j]+" ");
+                if(j == 2 || j == 5)
+                    System.out.print("| ");
+                }
+                    System.out.println();
+                if(i==2 || i==5)
+                    System.out.println("---------------------");
+            }
+        System.out.println();
     }
 }
