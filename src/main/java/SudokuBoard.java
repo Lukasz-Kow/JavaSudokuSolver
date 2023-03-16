6540ed280f4f5dcd155ca0817bb25e08672196a2
Łukasz Kowalczyk <242163@edu.p.lodz.pl>
2023-03-16 12:53:27 +0100

    checkstyle2023 update

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -18,30 +18,29 @@ public class SudokuBoard {
     }
 
     public boolean fillBoard(int row, int column) {
-        if(row == 8 && column == 9) {
+        if (row == 8 && column == 9) {
             return true;
         }
 
-        if(column == 9) {
+        if (column == 9) {
             row++;
             column = 0;
         }
 
 
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
 
             // Using a random number generator to fill the board
             // Not optimal but it works
             // TODO: Implement something with an array that is randomized everry time and iterate through it
             int randomNumber = new Random().nextInt(9) + 1;
 
-            if(isValid(row, column, randomNumber)) {
+            if (isValid(row, column, randomNumber)) {
                 board[row][column] = randomNumber;
 
-                if(fillBoard(row, column + 1)) {
+                if (fillBoard(row, column + 1)) {
                     return true;
-                }
-                else{
+                } else {
                     board[row][column] = 0;
                 }
             }
@@ -75,13 +74,15 @@ public class SudokuBoard {
     public void printBoard() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j]+" ");
-                if(j == 2 || j == 5)
+                System.out.print(board[i][j] + " ");
+                if (j == 2 || j == 5) {
                     System.out.print("| ");
                 }
+                }
                     System.out.println();
-                if(i==2 || i==5)
+                if (i == 2 || i == 5) {
                     System.out.println("---------------------");
+                }
             }
         System.out.println();
     }
