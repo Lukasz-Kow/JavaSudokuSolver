6f63de670decea763ccb328e2f2d1375fac1fa5a
MateuszPryl <mateusz.pryl2@gmail.com>
2023-06-08 14:58:08 +0200

    Updated pom.xml files

--- ViewProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ife_mp_lk.viewproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens ife_mp_lk.viewproject to javafx.fxml;
-    exports ife_mp_lk.viewproject;
-}
